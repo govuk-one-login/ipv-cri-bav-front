9079e2b1d637b4c3e5c060b3785236a15e2c39a5
Stelios_CB_GDS <stelios.constantinou-briggs@digital.cabinet-office.gov.uk>
2023-10-27 13:10:44 +0100

    Remove uneccesary validations

--- src/app/bav/fields.js
+++ src/app/bav/fields.js
@@ -2,27 +2,13 @@ module.exports = {
   surname: {
     type: "text",
     journeyKey: "surname",
-    validate: [
-      "required",
-      { type: "regexSpecialCharacters", fn: (value) => value.match(/^[A-Za-z0-9 .'-]*$/) },
-      { type: "regexNumber", fn: (value) => value.match(/^[a-zA-Z .'-]*$/) }
-    ]
   },
   firstName: {
     type: "text",
     journeyKey: "firstName",
-    validate: [
-      "required",
-      { type: "regexSpecialCharacters", fn: (value) => value.match(/^[A-Za-z0-9 .'-]*$/) },
-      { type: "regexNumber", fn: (value) => value.match(/^[a-zA-Z .'-]*$/) }
-    ]
   },
   middleName: {
     type: "text",
     journeyKey: "middleName",
-    validate: [
-      { type: "regexSpecialCharacters", fn: (value) => value.match(/^[A-Za-z0-9 .'-]*$/) },
-      { type: "regexNumber", fn: (value) => value.match(/^[a-zA-Z .'-]*$/) }
-    ]
-  },
+  }
 };
