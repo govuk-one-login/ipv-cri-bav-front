3b935e7150aa7c2eff1939509c9a45679f585fe9
Z-Mohamoud <110032361+Z-Mohamoud@users.noreply.github.com>
2023-11-16 15:51:51 +0000

    Kiwi 1332 QA - Landing Page (#25)
    * KIWI-1332-QA
    
    
    ---------
    
    Co-authored-by: shenry99 <sadie.henry@digital.cabinet-office.gov.uk>

---
+++ test/browser/pages/abortPage.js
@@ -0,0 +1,18 @@
+module.exports = class PlaywrightDevPage {
+  /**
+   * @param {import('@playwright/test').Page} page
+   */
+  constructor(page) {
+    this.page = page;
+    this.path = "/abort";
+  }
+
+  async isCurrentPage() {
+    const { pathname } = new URL(this.page.url());
+    return pathname === this.path;
+  }
+
+};
+
+
+
---
+++ test/browser/pages/landingPage.js
@@ -0,0 +1,41 @@
+module.exports = class PlaywrightDevPage {
+  /**
+   * @param {import('@playwright/test').Page} page
+   */
+  constructor(page) {
+    this.page = page;
+    this.path = "/prove-identity-bank-account";
+  }
+
+  async isCurrentPage() {
+    const { pathname } = new URL(this.page.url());
+    return pathname === this.path;
+  }
+
+  async continueToAccountDetails() {
+    await this.page.click("#landingPageContinue");
+  }
+
+  async clickHowWeUseBankDetails() {
+    await this.page.click(".govuk-details__summary-text");
+  }
+
+  async clickFindOtherWaysToProveIdentity() {
+    await this.page.click('[href*="/abort"]');
+  }
+
+  async isHowWeUseBankDetailsSectionDisplayed() {
+    const { expect } = require("@playwright/test");
+    expect(await this.page.locator(".instruction:nth-child(1)").textContent()).toEqual("We will not save or share your account details.")
+    expect(await this.page.locator(".instruction:nth-child(2)").textContent()).toEqual("We do not use your account details to:")
+  }
+
+  async checkErrorText() {
+    const errorText = await this.page.locator("#error-summary-title").textContent();
+    return errorText.trim();
+  }
+
+};
+
+
+
