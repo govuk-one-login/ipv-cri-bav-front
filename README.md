81f34afe1e632d2452d1cc1619b5411251ccb562
Stelios_CB_GDS <stelios.constantinou-briggs@digital.cabinet-office.gov.uk>
2023-11-23 14:51:16 +0000

    Fix prettier issues

--- README.md
+++ README.md
@@ -53,6 +53,7 @@ yarn build
 yarn install
 yarn test:browser:ci
 ```
+
 ### Code Owners
 
 This repo has a `CODEOWNERS` file in the root and is configured to require PRs to reviewed by Code Owners.
--- test/utils/multiReportGenerator.js
+++ test/utils/multiReportGenerator.js
@@ -1,30 +1,50 @@
 const reporter = require("multiple-cucumber-html-reporter");
 var date = new Date();
-var currentDate = date.getDate() + '_' +( date.getMonth()+1) + '_' + date.getFullYear() + '_' + date.getHours() + '_' + date.getMinutes() + '_' + date.getSeconds() + '_' + date.getMilliseconds();
+var currentDate =
+  date.getDate() +
+  "_" +
+  (date.getMonth() + 1) +
+  "_" +
+  date.getFullYear() +
+  "_" +
+  date.getHours() +
+  "_" +
+  date.getMinutes() +
+  "_" +
+  date.getSeconds() +
+  "_" +
+  date.getMilliseconds();
 
 var options = {
-    jsonDir: 'test/reports',
-    reportPath: 'test/reports/bav-reports/cri-bav-report_'+currentDate + '.html',
-    metadata: {
-        browser: {
-          name: "chrome",
-          version: "112.0.56",
-        },
-        device: "MacBook",
-        platform: {
-          name: "osx",
-          version: "Ventura 13.3.1",
-        },
+  jsonDir: "test/reports",
+  reportPath:
+    "test/reports/bav-reports/cri-bav-report_" + currentDate + ".html",
+  metadata: {
+    browser: {
+      name: "chrome",
+      version: "112.0.56",
+    },
+    device: "MacBook",
+    platform: {
+      name: "osx",
+      version: "Ventura 13.3.1",
+    },
+  },
+  disableLog: "true",
+  openReportInBrowser: "true",
+  customData: {
+    title: "Run info",
+    data: [
+      {
+        label: "Project:",
+        value: "Digital Identity: Bank Account as Verification",
       },
-    disableLog: 'true',
-    openReportInBrowser: 'true',
-    customData: {
-        title: "Run info",
-        data: [
-          { label: "Project:", value: "Digital Identity: Bank Account as Verification" },
-          { label: "Environment:", value: "Local" },
-          { label: "Execution Start Time: ", value: (new Date()).toString().substring(0,25)},
-        ],
+      { label: "Environment:", value: "Local" },
+      {
+        label: "Execution Start Time: ",
+        value: new Date().toString().substring(0, 25),
       },
+    ],
+  },
 };
 reporter.generate(options);
