9177b3b240b742ab63f70e2525b02f40ccdb0715
George Dainton <george.dainton@digital.cabinet-office.gov.uk>
2023-11-24 11:39:22 +0000

    Sort code formatting

--- src/app/bav/controllers/confirmDetails.js
+++ src/app/bav/controllers/confirmDetails.js
@@ -1,5 +1,24 @@
 const BaseController = require("hmpo-form-wizard").Controller;
 
-class ConfirmDetailsController extends BaseController {}
+class ConfirmDetailsController extends BaseController {
+    locals(req, res, callback) {
+        super.locals(req, res, (err, locals) => {
+            if (err) {
+              return callback(err, locals);
+            }
+
+    const sortCode = req.form.values.sortCode.split("").filter((char) => char != " " && char != "-")
+    sortCode.splice(2, 0, "-")
+    sortCode.splice(5, 0, "-")
+    const displaySortCode = sortCode.join("")
+    
+    locals.sortCode = displaySortCode
+    locals.accountNumber = req.form.values.accountNumber
+    
+    callback(err, locals);
+    })
+}}
+
+
 
 module.exports = ConfirmDetailsController; 
