4e754fb5daf30f74afb5e0f0c10edd6faa97b40e
George Dainton <george.dainton@digital.cabinet-office.gov.uk>
2023-11-30 17:10:17 +0000

    wip

--- src/app/bav/controllers/confirmDetails.js
+++ src/app/bav/controllers/confirmDetails.js
@@ -7,6 +7,19 @@ class ConfirmDetailsController extends BaseController {
               return callback(err, locals);
             }
 
+            const sharedClaims = req.session?.shared_claims;
+            console.log("---------",sharedClaims)
+        
+            if (sharedClaims && sharedClaims?.name?.length > 0) {
+                let names = sharedClaims.name[0].nameParts
+                console.log("------", names[0].value)
+                console.log("------", names[names.length-2].value)
+                console.log("------", names[names.length-1].value)
+                req.sessionModel.set("firstName", names[0].value);
+                req.sessionModel.set("middleName", names[names.length-2].value);
+                req.sessionModel.set("lastName", names[names.length-1].value);
+            }
+
     const sortCode = req.form.values.sortCode.split("").filter((char) => char != " " && char != "-")
     sortCode.splice(2, 0, "-")
     sortCode.splice(5, 0, "-")
--- src/app/bav/controllers/root.js
+++ src/app/bav/controllers/root.js
@@ -2,14 +2,37 @@ const { Controller: BaseController } = require("hmpo-form-wizard");
 
 class RootController extends BaseController {
   async saveValues(req, res, next) {
-    const sharedClaims = req.session?.shared_claims;
+
+  //Below code is in anticipation of design changing to a single given name field
+    
+  //   const sharedClaims = req.session?.shared_claims;
+  //   const entries = sharedClaims.name[0].nameParts
+
+  //   const getFullName = () => {
+  //     let nameParts = []
+  //     for(const entry of entries) {
+  //       nameParts.push(entry.value)
+  //     }
+  //     const fullName = nameParts.join(" ")
+  //     req.sessionModel.set("fullName", fullName)
+  //   }
+
+  //   getFullName()
+  // }
+
+  
+  const sharedClaims = req.session?.payload.shared_claims;
 
     if (sharedClaims && sharedClaims?.name?.length > 0) {
         let names = sharedClaims.name[0].nameParts
+        console.log("------", names[0].value)
+        console.log("------", names[names.length-2].value)
+        console.log("------", names[names.length-1].value)
         req.sessionModel.set("firstName", names[0].value);
         req.sessionModel.set("middleName", names[names.length-2].value);
         req.sessionModel.set("lastName", names[names.length-1].value);
     }
+    
     super.saveValues(req, res, next);
   }
 }
