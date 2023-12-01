a692d708c16c491bf586435f4d4568fa7dba5ac6
George Dainton <george.dainton@digital.cabinet-office.gov.uk>
2023-12-01 12:15:04 +0000

    Placeholder value for fullName pending lambda

--- src/app/bav/controllers/confirmDetails.js
+++ src/app/bav/controllers/confirmDetails.js
@@ -6,35 +6,21 @@ class ConfirmDetailsController extends BaseController {
             if (err) {
               return callback(err, locals);
             }
-
-            const sharedClaims = req.session?.shared_claims;
-            console.log("---------",sharedClaims)
-        
-            if (sharedClaims && sharedClaims?.name?.length > 0) {
-                let names = sharedClaims.name[0].nameParts
-                console.log("------", names[0].value)
-                console.log("------", names[names.length-2].value)
-                console.log("------", names[names.length-1].value)
-                req.sessionModel.set("firstName", names[0].value);
-                req.sessionModel.set("middleName", names[names.length-2].value);
-                req.sessionModel.set("lastName", names[names.length-1].value);
-            }
-
-    const sortCode = req.form.values.sortCode.split("").filter((char) => char != " " && char != "-")
-    sortCode.splice(2, 0, "-")
-    sortCode.splice(5, 0, "-")
-    const displaySortCode = sortCode.join("")
     
-    locals.firstName = req.sessionModel.get("firstName")
-    locals.middleName = req.sessionModel.get("middleName")
-    locals.lastName = req.sessionModel.get("lastName")
-    locals.sortCode = displaySortCode
+    const formatSortCode = () => {
+      const sortCode = req.form.values.sortCode.split("").filter((char) => char != " " && char != "-")
+      sortCode.splice(2, 0, "-")
+      sortCode.splice(5, 0, "-")
+      const displaySortCode = sortCode.join("")
+      locals.sortCode = displaySortCode
+    }
+    
+    formatSortCode()
+    locals.fullName = req.sessionModel.get("fullName")
     locals.accountNumber = req.form.values.accountNumber
     
     callback(err, locals);
     })
 }}
 
-
-
-module.exports = ConfirmDetailsController; 
+module.exports = ConfirmDetailsController;
--- src/app/bav/controllers/root.js
+++ src/app/bav/controllers/root.js
@@ -3,36 +3,10 @@ const { Controller: BaseController } = require("hmpo-form-wizard");
 class RootController extends BaseController {
   async saveValues(req, res, next) {
 
-  //Below code is in anticipation of design changing to a single given name field
-    
-  //   const sharedClaims = req.session?.shared_claims;
-  //   const entries = sharedClaims.name[0].nameParts
+  //TODO: Call new fetchNameInfo Lambda to users name as single string
+		// req.sessionModel.set("fullName", data.full_name);
 
-  //   const getFullName = () => {
-  //     let nameParts = []
-  //     for(const entry of entries) {
-  //       nameParts.push(entry.value)
-  //     }
-  //     const fullName = nameParts.join(" ")
-  //     req.sessionModel.set("fullName", fullName)
-  //   }
-
-  //   getFullName()
-  // }
-
-  
-  const sharedClaims = req.session?.payload.shared_claims;
-
-    if (sharedClaims && sharedClaims?.name?.length > 0) {
-        let names = sharedClaims.name[0].nameParts
-        console.log("------", names[0].value)
-        console.log("------", names[names.length-2].value)
-        console.log("------", names[names.length-1].value)
-        req.sessionModel.set("firstName", names[0].value);
-        req.sessionModel.set("middleName", names[names.length-2].value);
-        req.sessionModel.set("lastName", names[names.length-1].value);
-    }
-    
+		req.sessionModel.set("fullName", "John Peter Smith");
     super.saveValues(req, res, next);
   }
 }
