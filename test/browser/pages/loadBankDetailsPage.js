21deef48a5241dcab2962e86444c20e2ba9ee19e
shenry99 <115989182+shenry99@users.noreply.github.com>
2023-12-06 12:26:00 +0000

    KIWI-BAV-1336  Happy & Unhappy Path tests for CYA (#49)
    * KIWI-BAV-1336  Happy & Unhappy Path tests for CYA
    
    * KIWI-BAV-1336 Format and coding issues resolved
    
    * KIWI-BAV-1336 Steps file updated
    
    * KIWI-BAV-1336 BDD files updated for consistency.

--- test/browser/pages/loadBankDetailsPage.js
+++ test/browser/pages/loadBankDetailsPage.js
@@ -4,20 +4,12 @@ module.exports = class PlaywrightDevPage {
    */
   constructor(page) {
     this.page = page;
-    this.path = "/confirm-details";
+    this.path = "/load-bank-details";
   }
 
   async isCurrentPage() {
     const { pathname } = new URL(this.page.url());
     return pathname === this.path;
-  }
-
-  async back() {
-    await this.page.click("#back");
-  }
-
-  async goBack() {
-    await this.page.goBack();
   }
 
   async clickChangeLink() {
@@ -29,9 +21,5 @@ module.exports = class PlaywrightDevPage {
 
   async clickDoNotContinueToBankDetailsCheck() {
     await this.page.locator('[href*="/abort"]').click();
-  }
-
-  async clickSubmitDetailsButton() {
-    await this.page.locator("#submitDetails").click();
   }
 };
---
+++ test/browser/step_definitions/BAVCheckYourDetailSteps.step.js
@@ -0,0 +1,87 @@
+const { Given, Then, When } = require("@cucumber/cucumber");
+const {
+  ConfirmDetailsPage,
+  AccountDetailsEditPage,
+  AbortPage,
+  LoadBankDetailsPage,
+  AccountDetailsPage,
+} = require("../pages");
+const { expect } = require("@playwright/test");
+
+Given("the user wishes to proceed", async function () {
+  const cyaPage = new ConfirmDetailsPage(await this.page);
+  expect(await cyaPage.isCurrentPage()).toBeTruthy();
+});
+
+When(
+  "they click on the Continue to bank details check button",
+  async function () {
+    const cyaPage = new ConfirmDetailsPage(await this.page);
+    await cyaPage.clickSubmitDetailsButton();
+  }
+);
+
+When(
+  "the user clicks the Change button to change their Sort code or Account number",
+  async function () {
+    const cyaPage = new ConfirmDetailsPage(await this.page);
+    await cyaPage.clickChangeLink();
+  }
+);
+
+When(
+  "the user edits the sort code {string} and the account number {string}",
+  async function (sortCode, accountNo) {
+    const accDetailsEditPage = new AccountDetailsEditPage(await this.page);
+    await accDetailsEditPage.editSortCode(sortCode);
+    await accDetailsEditPage.editAccountNumber(accountNo);
+    await accDetailsEditPage.continueButton();
+  }
+);
+
+When("the user clicks the “Back” link on the CYA page", async function () {
+  const cyaPage = new ConfirmDetailsPage(await this.page);
+  await cyaPage.back();
+});
+
+When(
+  "the user clicks the browser Back button on the CYA page",
+  async function () {
+    const cyaPage = new ConfirmDetailsPage(await this.page);
+    await cyaPage.goBack();
+  }
+);
+
+When(
+  "they click on the “I do not want to continue to bank details check” link",
+  async function () {
+    const cyaPage = new ConfirmDetailsPage(await this.page);
+    await cyaPage.clickDoNotContinueToBankDetailsCheck();
+  }
+);
+
+Then(
+  "the user is directed to the Loading Bank Details screen",
+  async function () {
+    const loadBankDetails = new LoadBankDetailsPage(await this.page);
+    await loadBankDetails.isCurrentPage();
+  }
+);
+
+Then(
+  "the user is redirected to the check your details page",
+  async function () {
+    const cyaPage = new ConfirmDetailsPage(await this.page);
+    expect(await cyaPage.isCurrentPage()).toBeTruthy();
+  }
+);
+
+Then("they are routed to the Account Details Page", async function () {
+  const accDetailsPage = new AccountDetailsPage(await this.page);
+  expect(await accDetailsPage.isCurrentPage()).toBeTruthy();
+});
+
+Then("the user is directed to the Escape choice screen", async function () {
+  const abortPage = new AbortPage(await this.page);
+  await abortPage.isCurrentPage();
+});
