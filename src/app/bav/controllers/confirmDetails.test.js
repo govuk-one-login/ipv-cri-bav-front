24d53328f8b3b7e91f65e32fd454903ad7ed48f4
George Dainton <george.dainton@digital.cabinet-office.gov.uk>
2023-12-15 15:58:04 +0000

    Unit tests wip

--- src/app/bav/controllers/confirmDetails.test.js
+++ src/app/bav/controllers/confirmDetails.test.js
@@ -1,12 +1,26 @@
 const BaseController = require("hmpo-form-wizard").Controller;
 const ConfirmDetailsController = require("./confirmDetails");
+const { setupDefaultMocks } = require("../../../../test/utils/test-helpers");
 
 describe("ConfirmDetailsController", () => {
-  const confirmDetailsController = new ConfirmDetailsController({
-    route: "/test",
+  let confirmDetailsController;
+  let req;
+  let res;
+  
+  beforeEach(() => {
+    const setup = setupDefaultMocks();
+    req = setup.req;
+    res = setup.res;
+
+  confirmDetailsController = new ConfirmDetailsController({ route: "/test" });
   });
 
   it("should be an instance of BaseController", () => {
     expect(confirmDetailsController).toBeInstanceOf(BaseController);
   });
+
+  // it("should set the isLanding sessionModel property to false", () => {
+  //   confirmDetailsController.locals(req, res)
+  //   expect(req.sessionModel.get("isLanding")).toEqual(false)
+  // });
 });
--- src/app/bav/controllers/landingPage.test.js
+++ src/app/bav/controllers/landingPage.test.js
@@ -1,10 +1,26 @@
 const BaseController = require("hmpo-form-wizard").Controller;
 const LandingPageController = require("./landingPage");
+const { setupDefaultMocks } = require("../../../../test/utils/test-helpers");
 
 describe("LandingPageController", () => {
-  const landingPageController = new LandingPageController({ route: "/test" });
+  let landingPageController;
+  let req;
+  let res;
+  
+  beforeEach(() => {
+    const setup = setupDefaultMocks();
+    req = setup.req;
+    res = setup.res;
+
+    landingPageController = new LandingPageController({ route: "/test" });
+  });
 
   it("should be an instance of BaseController", () => {
     expect(landingPageController).toBeInstanceOf(BaseController);
+  });
+
+  it("should set the isLanding sessionModel property to true", () => {
+    landingPageController.locals(req, res)
+    expect(req.sessionModel.get("isLanding")).toEqual(true)
   });
 });
