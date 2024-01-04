11b183ac67a372b43005023af3d1591e213db9c0
Z-Mohamoud <110032361+Z-Mohamoud@users.noreply.github.com>
2024-01-04 09:56:43 +0000

    KIWI-1409 QA Updates (#65)
    * KIWI-1409 QA Updates

---
+++ test/browser/step_definitions/BAVErrorPageSteps.step.js
@@ -0,0 +1,26 @@
+const { When, Then } = require("@cucumber/cucumber");
+const { ErrorPage } = require("../pages");
+
+When("the user navigates to a URL that returns an Error", async function () {
+  const errorPage = new ErrorPage(await this.page);
+  const errorPageUrl = (await errorPage.returnUrl()).href.replace(
+    "prove-identity-bank-account",
+    "error"
+  );
+  await errorPage.navigateToUrl(errorPageUrl);
+});
+
+Then("the user is directed to the Internal Error Page", async function () {
+  const errorPage = new ErrorPage(await this.page);
+  await errorPage.validateErrorScreen();
+});
+
+When("the user clicks the Gov.UK Homepage button", async function () {
+  const errorPage = new ErrorPage(await this.page);
+  await errorPage.clickGovUkHomeButton();
+});
+
+Then("the user is routed to the Gov.UK Homepage", async function () {
+  const errorPage = new ErrorPage(await this.page);
+  await errorPage.isGovUkHomepage();
+});
