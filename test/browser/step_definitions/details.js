6dae7674d702ad4dbcfbd30abda30e7770a0efca
Jack Mardell <jack.mardell@digital.cabinet-office.gov.uk>
2024-01-15 16:49:02 +0000

    Ran prettier and lint

--- test/browser/step_definitions/details.js
+++ test/browser/step_definitions/details.js
@@ -2,14 +2,11 @@ const { Given } = require("@cucumber/cucumber");
 const { RelyingPartyPage, BavLandingPage } = require("../pages");
 const { expect } = require("@playwright/test");
 
-Given(
-  /^([^"]*) has navigated to the BAV Landing Page$/,
-  async function (name) {
-    const claim = this.allUserClaims[name];
-    const rpPage = new RelyingPartyPage(this.page);
-    const bavLandingPage = new BavLandingPage(this.page);
+Given(/^([^"]*) has navigated to the BAV Landing Page$/, async function (name) {
+  const claim = this.allUserClaims[name];
+  const rpPage = new RelyingPartyPage(this.page);
+  const bavLandingPage = new BavLandingPage(this.page);
 
-    await rpPage.goto(claim);
-    expect(await bavLandingPage.isCurrentPage()).toBeTruthy();
-  }
-);
+  await rpPage.goto(claim);
+  expect(await bavLandingPage.isCurrentPage()).toBeTruthy();
+});
