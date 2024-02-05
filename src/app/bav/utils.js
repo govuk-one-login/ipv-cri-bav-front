deed66eef83e3cb9f74313f83749271ef3cce3fa
Caitlin Cooling <caitlin.cooling@digital.cabinet-office.gov.uk>
2024-02-05 10:55:21 +0000

    KIWI-1401: Stop formatting sort code

--- src/app/bav/utils.js
+++ src/app/bav/utils.js
@@ -1,15 +1,5 @@
-function formatSortCode(sortCode) {
-  const splitSortCode = sortCode
-    .split("")
-    .filter((char) => char != " " && char != "-");
-  splitSortCode.splice(2, 0, "-");
-  splitSortCode.splice(5, 0, "-");
-  const displaySortCode = splitSortCode.join("");
-  return displaySortCode;
-}
-
 function formatSortCodeForSubmission(sortCode) {
   return sortCode.replace(/[ -]/g, "");
 }
 
-module.exports = { formatSortCode, formatSortCodeForSubmission };
+module.exports = { formatSortCodeForSubmission };
