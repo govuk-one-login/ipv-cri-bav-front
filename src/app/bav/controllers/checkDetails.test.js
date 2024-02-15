5f1a7c95ace4515a31f2ee3d44b322ca6cb8c7c7
Caitlin Cooling <caitlin.cooling@digital.cabinet-office.gov.uk>
2024-02-15 13:45:54 +0000

    KIWI-1401: Up test coverage

--- src/app/bav/controllers/checkDetails.test.js
+++ src/app/bav/controllers/checkDetails.test.js
@@ -2,6 +2,8 @@ const BaseController = require("hmpo-form-wizard").Controller;
 const CheckDetailsController = require("./checkDetails");
 const { setupDefaultMocks } = require("../../../../test/utils/test-helpers");
 const axios = require("axios");
+const { API } = require("../../../lib/config");
+
 jest.mock("axios");
 
 describe("CheckDetailsController", () => {
@@ -23,36 +25,56 @@ describe("CheckDetailsController", () => {
     expect(checkDetailsController).toBeInstanceOf(BaseController);
   });
 
-  it("should set the isLanding sessionModel property to false", () => {
-    req.form.values.sortCode = "123456";
-    checkDetailsController.locals(req, res, callback);
-    expect(req.sessionModel.get("isLanding")).toEqual(false);
+  describe("#locals", () => {
+    it("should set the isLanding sessionModel property to false", () => {
+      req.form.values.sortCode = "123456";
+      checkDetailsController.locals(req, res, callback);
+      expect(req.sessionModel.get("isLanding")).toEqual(false);
+    });
   });
 
-  it("should increment the attemptCount sessionModel property when attemptCount returned in API call", async () => {
-    axios.post.mockResolvedValue({
-      data: {
-        message: "Success",
-        attemptCount: 1,
-      },
+  describe("#saveValues", () => {
+    it("should save the BAV data", async () => {
+      jest.spyOn(req.axios, "post");
+      jest.spyOn(req.sessionModel, "get").mockReturnValueOnce("00-00-00");
+      jest.spyOn(req.sessionModel, "get").mockReturnValueOnce("1234567");
+
+      await checkDetailsController.saveValues(req, res, callback);
+
+      expect(req.axios.post).toHaveBeenCalledWith(
+        `${API.PATHS.SAVE_BAVDATA}`,
+        { sort_code: "000000", account_number: "1234567" },
+        { headers: { "x-govuk-signin-session-id": req.session.tokenId } }
+      );
     });
-
-    const bavData = {};
-    await checkDetailsController.saveBavData(axios, bavData, req);
-
-    expect(req.sessionModel.get("attemptCount")).toEqual(1);
   });
 
-  it("should return undefined for attemptCount sessionModel property when attemptCount returned in API call as undefined", async () => {
-    axios.post.mockResolvedValue({
-      data: {
-        message: "Success",
-      },
+  describe("#saveBavData", () => {
+    it("should increment the attemptCount sessionModel property when attemptCount returned in API call", async () => {
+      axios.post.mockResolvedValue({
+        data: {
+          message: "Success",
+          attemptCount: 1,
+        },
+      });
+
+      const bavData = {};
+      await checkDetailsController.saveBavData(axios, bavData, req);
+
+      expect(req.sessionModel.get("attemptCount")).toEqual(1);
     });
 
-    const bavData = {};
-    await checkDetailsController.saveBavData(axios, bavData, req);
+    it("should return undefined for attemptCount sessionModel property when attemptCount returned in API call as undefined", async () => {
+      axios.post.mockResolvedValue({
+        data: {
+          message: "Success",
+        },
+      });
 
-    expect(req.sessionModel.get("attemptCount")).toEqual(undefined);
+      const bavData = {};
+      await checkDetailsController.saveBavData(axios, bavData, req);
+
+      expect(req.sessionModel.get("attemptCount")).toEqual(undefined);
+    });
   });
 });
