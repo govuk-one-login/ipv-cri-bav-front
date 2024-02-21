629e0f2ab976dd44993ac2f5b32f6c1544c30d0e
Zac Mohamoud <zac.mohamoud@digital.cabinet-office.gov.uk>
2024-02-21 10:53:21 +0000

    Update .env logic for BAV

--- test/browser/step_definitions/details.js
+++ test/browser/step_definitions/details.js
@@ -3,6 +3,7 @@ const { RelyingPartyPage, BavLandingPage } = require("../pages");
 const { expect } = require("@playwright/test");
 
 Given(/^([^"]*) has navigated to the BAV Landing Page$/, async function (name) {
+  require('dotenv').config()
   const claim = this.allUserClaims[name];
   const rpPage = new RelyingPartyPage(this.page);
   const bavLandingPage = new BavLandingPage(this.page);
