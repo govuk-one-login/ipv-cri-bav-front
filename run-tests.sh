fe8c2d2f04b605ee71d24f292179f938da516e0b
Z-Mohamoud <110032361+Z-Mohamoud@users.noreply.github.com>
2024-02-23 12:29:37 +0000

    BAV .env updates (#140)
    * BAV .env updates

--- run-tests.sh
+++ run-tests.sh
@@ -11,13 +11,10 @@ remove_quotes () {
 # Github actions set to true for tests to run in headless mode
 export GITHUB_ACTIONS=true
 # shellcheck disable=SC2154
-export API_BASE_URL=http://localhost:8090
+export API_BASE_URL=$(remove_quotes "$CFN_BAVBackEndURL")
 export IPV_STUB_URL=$(remove_quotes $CFN_BAVIPVStubExecuteURL)start
-export IPV_BASE_URL=https://$(remove_quotes $CFN_BAVCustomDomain)
 export TEST_HARNESS_URL=$(remove_quotes $CFN_BAVTestHarnessURL)
-export SESSION_TABLE=$(remove_quotes $CFN_BackendSessionTableName)
-export BAV_API_URL=$(remove_quotes "$CFN_BAVBackEndURL")
-
+export SESSION_TABLE=$(remove_quotes $CFN_BAVBackendSessionTableName)
 
 cd /app; yarn run test:e2e:cd
 
