e2b03524de49346cee4cbdff809e9050ce87043c
Zac Mohamoud <zac.mohamoud@digital.cabinet-office.gov.uk>
2024-03-07 10:04:26 +0000

    remove redundant files + logic

--- test/browser/step_definitions/details.js
+++ test/browser/step_definitions/details.js
@@ -2,10 +2,10 @@ const { Given } = require("@cucumber/cucumber");
 const { RelyingPartyPage, BavLandingPage } = require("../pages");
 const { expect } = require("@playwright/test");
 
-Given(/^([^"]*) has navigated to the BAV Landing Page$/, async function (name) {
-  const claim = this.allUserClaims[name];
+Given(/^A user has navigated to the BAV Landing Page$/, async function () {
   const rpPage = new RelyingPartyPage(this.page);
   const bavLandingPage = new BavLandingPage(this.page);
+  const claim = require("../support/shared_claim.json");
 
   await rpPage.goto(claim);
   expect(await bavLandingPage.isCurrentPage()).toBeTruthy();
