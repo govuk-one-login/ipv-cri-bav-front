e03e45088fdbb873d1046ba3f035e96acb9d609f
di-fabs <fabien.saujot@digital.cabinet-office.gov.uk>
2024-03-07 11:43:25 +0000

    add language toggle feature

--- src/lib/config.js
+++ src/lib/config.js
@@ -36,6 +36,7 @@ module.exports = {
       GA4_DISABLED: process.env.GA4_DISABLED || false,
       UA_DISABLED: process.env.UA_DISABLED || true,
     },
+    LANGUAGE_TOGGLE_ENABLED: process.env.LANGUAGE_TOGGLE_ENABLED || false,
   },
   PORT: process.env.PORT || 5040,
   SESSION_SECRET: process.env.SESSION_SECRET,
