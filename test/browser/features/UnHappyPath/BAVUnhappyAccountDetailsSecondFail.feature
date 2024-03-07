9ebca3b4b4ff51731920050dec33e792001f1f8f
Zac Mohamoud <zac.mohamoud@digital.cabinet-office.gov.uk>
2024-03-07 10:08:08 +0000

    update negative testing logic

--- test/browser/features/UnHappyPath/BAVUnhappyAccountDetailsSecondFail.feature
+++ test/browser/features/UnHappyPath/BAVUnhappyAccountDetailsSecondFail.feature
@@ -8,7 +8,7 @@ Feature: Provided Bank Details Failed
 
     Scenario:  User failed on first attempt then failed on second attempt and is directed to IPV
         Given the user has entered a Sort Code of "123456"
-        Given the user has entered an Account Number of "00111111"
+        Given the user has entered an Account Number of "22222222"
         When the user clicks the Continue button
         When they click on the Continue to account details check button
         When the user selects the 'Try Again' radio
--- test/browser/step_definitions/details.js
+++ test/browser/step_definitions/details.js
@@ -2,7 +2,7 @@ const { Given } = require("@cucumber/cucumber");
 const { RelyingPartyPage, BavLandingPage } = require("../pages");
 const { expect } = require("@playwright/test");
 
-Given(/^A user has navigated to the BAV Landing Page$/, async function () {
+Given(/^a user has navigated to the BAV Landing Page$/, async function () {
   const rpPage = new RelyingPartyPage(this.page);
   const bavLandingPage = new BavLandingPage(this.page);
   const claim = require("../support/shared_claim.json");
@@ -10,3 +10,5 @@ Given(/^A user has navigated to the BAV Landing Page$/, async function () {
   await rpPage.goto(claim);
   expect(await bavLandingPage.isCurrentPage()).toBeTruthy();
 });
+
+
