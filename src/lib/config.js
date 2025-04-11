f996356303aeb4559b4ca0fcf2a1ab10e7f79274
Stelios_CB_GDS <stelios.constantinou-briggs@digital.cabinet-office.gov.uk>
2025-04-11 13:43:57 +0100

    Lint fix

--- src/lib/config.js
+++ src/lib/config.js
@@ -46,7 +46,8 @@ module.exports = {
         process.env.GA4_SELECT_CONTENT_ENABLED || true,
     },
     LANGUAGE_TOGGLE_DISABLED: process.env.LANGUAGE_TOGGLE_DISABLED || true,
-    DEVICE_INTELLIGENCE_ENABLED: process.env.DEVICE_INTELLIGENCE_ENABLED || false,
+    DEVICE_INTELLIGENCE_ENABLED:
+      process.env.DEVICE_INTELLIGENCE_ENABLED || false,
     DEVICE_INTELLIGENCE_DOMAIN: process.env.FRONTEND_DOMAIN || "localhost",
   },
   PORT: process.env.PORT || 5040,
